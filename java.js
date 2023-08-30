bec2c26c0d8f3c199b9b8fd38664649a7a72ac4f
ah4170537 <ah4710537@gmail.com>
2023-08-30 22:11:15 +0500

    cal

---
+++ java.js
@@ -0,0 +1,40 @@
+var screen = document.getElementById("screen");
+var button = document.querySelectorAll(".cal");
+for (item of button) {
+    item.addEventListener('click',(e)=>{
+       
+       if (e.target.innerHTML=='AC') {
+        screen.value=null;
+       }
+      else if (e.target.innerHTML=='x') {
+        screen.value= screen.value + '*';
+       }
+       else if (e.target.innerHTML=='=') {
+       try{
+        screen.value=eval(screen.value);
+       }
+       catch{
+        screen.value= 'ERROR'
+       }
+       }
+       else{
+        screen.value += e.target.innerHTML;
+       }
+    })
+}
+function remove()
+{
+    screen.value = screen.value.slice(0,-1);
+}
+function change(){
+    if(screen.value<0){
+        screen.value=-1 * screen.value;
+    }
+    else if(screen.value>0){
+        screen.value=-1 * screen.value;
+    }
+    else{
+        screen.value= screen.value;
+    }
+
+}
